49dd661e660541f4a1991ef952acc6651f4654e5
ryan johnson <ryanjohnson2794@yahoo.com>
2016-01-30 13:36:27 -0600

    Added basic scenes folder/pause menu/main

---
+++ New Unity Project 1/Assets/UIManagerScript.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class UIManagerScript : MonoBehaviour {
+
+	public void StartGame(int level) {
+		SceneManager.LoadScene (level);
+	}
+}
---
+++ New Unity Project 1/Assets/script/loadonclick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadonclick : MonoBehaviour {
+
+	public void LoadScene(int level) {
+		SceneManager.LoadScene (level);
+	}
+}
